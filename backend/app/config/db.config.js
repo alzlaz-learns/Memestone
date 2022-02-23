0bbf673dc157731b79a06588b6cbfbeefe98ca63
test user <test@test.com>
2022-02-22 18:42:35 -0600

    local use dbconfig

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "txaw3zs",
-  DB: "memestone_dev",
+  USER: "user1",
+  PASSWORD: "password1",
+  DB: "testdb",
   dialect: "mysql",
   pool: {
     max: 5,
